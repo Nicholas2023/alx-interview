d5ee83e36a2d36144c5bb4ca014ce358e610a82d
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2024-01-25 13:23:46 +0300

    task0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -15,17 +15,17 @@ def rotate_2d_matrix(matrix):
     if not all(map(lambda x: type(x) == list, matrix)):
         return
     rows = len(matrix)
-    column = len(matrix[0])
-    if not all(map(lambda x: len(x) == column, matrix)):
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
         return
     c, r = 0, rows - 1
-    for i in range(column * rows):
+    for i in range(cols * rows):
         if i % rows == 0:
             matrix.append([])
         if r == -1:
             r = rows - 1
             c += 1
-            matrix[-1].append(mtrix[r][c])
-            if c == column - 1 and r >= -1:
-                matrix.pop(r)
-            r -= 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
